5573604b43b1a81e94d74fa4d50a99bdad1733c7
Tyler Keller <tylerkeller@mines.edu>
2024-02-10 08:19:47 -0700

    update

---
+++ data-driven-diabetes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  data-driven-diabetes
+//
+//  Created by Tyler Keller on 2/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ data-driven-diabetes/data_driven_diabetesApp.swift
@@ -0,0 +1,17 @@
+//
+//  data_driven_diabetesApp.swift
+//  data-driven-diabetes
+//
+//  Created by Tyler Keller on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct data_driven_diabetesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
